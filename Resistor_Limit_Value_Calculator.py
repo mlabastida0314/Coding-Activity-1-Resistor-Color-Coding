823f9e4fb64c29e0c6f53e272ccae374ea0995f6
mlabastida0314 <146041806+mlabastida0314@users.noreply.github.com>
2024-02-09 21:34:42 +0800

    Update Resistor_Limit_Value_Calculator.py

--- Resistor_Limit_Value_Calculator.py
+++ Resistor_Limit_Value_Calculator.py
@@ -9,15 +9,20 @@ class ResistorLimitValueCalculator:
     def calculate_limits(self, color_band1, color_band2, color_band3, color_band4, color_band5=None):
         calculator_instance = ResistorNominalResistanceCalculator(color_band1, color_band2, color_band3, color_band4, color_band5)
 
-        result = calculator_instance.calculate_nominal_resistance()
-        if result is not None:
-            nominal_resistance, tolerance_value = result
-            min_limit_value = nominal_resistance - (nominal_resistance * float(tolerance_value) / 100)
-            max_limit_value = nominal_resistance + (nominal_resistance * float(tolerance_value) / 100)
+        try: 
+            result = calculator_instance.calculate_nominal_resistance()
+            if result is not None:
+                nominal_resistance, tolerance_value = result
+                min_limit_value = nominal_resistance - (nominal_resistance * float(tolerance_value) / 100)
+                max_limit_value = nominal_resistance + (nominal_resistance * float(tolerance_value) / 100)
 
-            return min_limit_value, max_limit_value
-        else:
-            print("Error in limit.py")
+                return min_limit_value, max_limit_value
+            else:
+                print("Error in limit.py")
+                return None
+                
+        except KeyError as e:
+            print(f"Error: Invalid color code entered - {e}")
             return None
 
 
