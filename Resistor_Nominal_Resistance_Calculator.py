061f3184cca96d67f145b1da917fcd5af6ca5b5a
blossom <mlabastida0314@gmail.com>
2024-02-10 12:10:41 +0800

    Update Resistor_Nominal_Resistance_Calculator.py

--- Resistor_Nominal_Resistance_Calculator.py
+++ Resistor_Nominal_Resistance_Calculator.py
@@ -1,25 +1,28 @@
 # nominal .py
 from Resistor_Color_Codes import color_codes, multipliers, tolerances
 
+#class
 class ResistorNominalResistanceCalculator:
-    def __init__(self, color_band1, color_band2, color_band3, color_band4, color_band5=None):
+    def __init__(self, color_band1, color_band2, color_band3, color_band4, color_band5=None): #initialization
         self.color_band1 = color_band1
         self.color_band2 = color_band2
         self.color_band3 = color_band3
         self.color_band4 = color_band4
         self.color_band5 = color_band5 if color_band5 else None
 
+    #method
     def calculate_nominal_resistance(self):
         code1 = color_codes[self.color_band1]
         code2 = color_codes[self.color_band2]
         code3 = color_codes[self.color_band3]
 
-        if self.color_band5 is not None:
+        #conditional statement
+        if self.color_band5 is not None: #if resistor has 5 bands
             code5 = tolerances[self.color_band5]
             multiplier = multipliers[self.color_band4]
             nominal_resistance = int(f"({code1}{code2}{code3}") * multiplier
             tolerance_value = float(code5)/100
-        else:
+        else: #otherwise, if resistor has 4 bands
             multiplier = multipliers[self.color_band3]
             nominal_resistance = (code1 + code2) * multiplier
             tolerance = tolerances[self.color_band4]
