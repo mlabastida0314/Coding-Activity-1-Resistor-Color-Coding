a34e2ef9ac92b6106fe55179dda574ba17163fce
aavelasco41 <aavelasco41@gmail.com>
2024-02-10 11:23:52 +0800

    git

--- Resistor_Limit_Value_Calculator.py
+++ Resistor_Limit_Value_Calculator.py
@@ -1,23 +1,25 @@
 # resistor limit.py
 from Resistor_Nominal_Resistance_Calculator import ResistorNominalResistanceCalculator
 
+# class
 class ResistorLimitValueCalculator:
-    def __init__(self):
+    def __init__(self): # initialization
         pass
 
+    # method
     def calculate_limits(self, color_band1, color_band2, color_band3, color_band4, color_band5=None):
         calculator_instance = ResistorNominalResistanceCalculator(color_band1, color_band2, color_band3, color_band4, color_band5)
-
+        # Error handling
         try:
             result = calculator_instance.calculate_nominal_resistance()
-            if result is not None:
+            if result is not None:  # if the var result have a value
                 nominal_resistance, tolerance_value = result
                 min_limit_value = nominal_resistance - (nominal_resistance * float(tolerance_value) / 100)
                 max_limit_value = nominal_resistance + (nominal_resistance * float(tolerance_value) / 100)
 
                 return min_limit_value, max_limit_value
             else:
-                return None, None
+                return None, None  # if var result doesnt have a value
         except KeyError as e:
             print(f"Error: Invalid color entered. - {e}")
             return None, None
